93306226fd635873b475c82444a1e40beecc12c5
Piotr Soluch <psoluch@wiredot.com>
2015-09-24 10:45:05 +0200

    svg plugin

--- lib/smarty-plugins/function.svg.php
+++ lib/smarty-plugins/function.svg.php
@@ -14,18 +14,13 @@
  *
  */
 function smarty_function_svg($params, $template) {
-	// $svg = new SimpleXMLElement( file_get_contents(get_bloginfo('template_directory').'/'.$params['file']) );
 
-	// $svg->registerXPathNamespace('svg', 'http://www.w3.org/2000/svg');
-	// $svg->registerXPathNamespace('xlink', 'http://www.w3.org/1999/xlink');
-
-
-	// $result = $svg->xpath('/svg:svg');
+	$svg = '<svg';
 	
-	//print_r($result);
-	$svg = '<svg class="fill-red">
-    <use xlink:href="/wp-content/themes/wd-media-szukajacboga-pl'.$params['file'].'"></use>
-</svg>';
-  return $svg;
-	return '<img src="'.get_bloginfo('template_url').'/'.$params['file'].'">'; 
+	if (isset($params['class'])) {
+		$svg.= ' class="'.$params['class'].'"';
+	}
+	
+	$svg.= '><use xlink:href="/wp-content/themes/'.get_template().$params['file'].'"></use></svg>';
+	return $svg;
 }
