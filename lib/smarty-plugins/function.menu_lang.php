e610f9009375227ae5daf1dd65a34d493f2690b2
Piotr Soluch <psoluch@wiredot.com>
2018-08-31 12:23:38 +0200

    improved language navigation

--- lib/smarty-plugins/function.menu_lang.php
+++ lib/smarty-plugins/function.menu_lang.php
@@ -14,14 +14,37 @@
  *
  */
 function smarty_function_menu_lang( $params, $template ) {
-
 	$navigation = '';
 
+	// default params
+	$default_params = array(
+		'class' => null,
+		'id' => null,
+		'url' => get_site_url(),
+	);
+
+	// merge default params with the provided ones
+	$params = array_merge( $default_params, $params );
+
 	if ( isset( CP::$config['language'] ) ) {
-		 $navigation .= '<ul>';
+		$navigation .= '<ul';
+
+		if ( $params['id'] ) {
+			$navigation .= ' id="' . $params['id'] . '"';
+		}
+
+		if ( $params['class'] ) {
+			$navigation .= ' class="' . $params['class'] . '"';
+		}
+
+		$navigation .= '>';
 		foreach ( CP::$config['language'] as $language ) {
 			$navigation .= '<li>';
-			$navigation .= '<a href="/?lang=' . $language['short_name'] . '">' . $language['name'] . '</a>';
+			$navigation .= '<a href="' . $params['url'] . '/' . $language['prefix'];
+			if ( $language['prefix'] ) {
+				$navigation .= '/';
+			}
+			$navigation .= '">' . $language['name'] . '</a>';
 
 			$navigation .= '</li>';
 		}
