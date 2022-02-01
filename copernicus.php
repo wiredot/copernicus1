8050c069b453b94d9695b8acc8765c92a142410d
Piotr Soluch <psoluch@wiredot.com>
2022-02-01 21:37:04 +0100

    updated version nr

--- copernicus.php
+++ copernicus.php
@@ -4,7 +4,7 @@
   Plugin URI:  http://copernicus.wiredot.com/
   Description: WordPress Framework
   Author: wiredot
-  Version: 1.2.0
+  Version: 1.3.0
   Author URI: http://wiredot.com/
   License: GPLv2 or later
  */
